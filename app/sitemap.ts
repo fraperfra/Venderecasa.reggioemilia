6d0cdeba12c704cbfee10229879fdfadfee8e7b8
fracop98 <info.mario.app@gmail.com>
2026-02-20 17:23:37 +0100

    feat: add sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from 'next';
+
+const BASE_URL = 'https://venderecasa.reggioemilia.it';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: BASE_URL,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${BASE_URL}/valutazione-gratuita-separazione`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_URL}/vendere-casa-mutuo`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_URL}/vendere-casa-trasferimento`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_URL}/casa-grande-valutazione`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_URL}/analisi-portafoglio-immobiliare`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: `${BASE_URL}/vendere-e-comprare-casa`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+    ];
+}
